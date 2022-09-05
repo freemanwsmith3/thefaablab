bad21eb55ca2661cf10e818c71170760ffeca86f
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-05 01:10:42 +0100

    sessions

--- faab/api/views.py
+++ faab/api/views.py
@@ -30,6 +30,7 @@ class GetWeek(APIView):
                 data = TargetSerializer(targets, many=True).data
                 #data['is_user'] = self.request.session.session_key == bid.host
                 
+                
                 return Response(data, status=status.HTTP_200_OK)
             return Response({'Bad Request':'Invalid Week'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request':'Week Not Provided'}, status=status.HTTP_404_NOT_FOUND)
@@ -57,14 +58,30 @@ class BidView(APIView):
 
                 bid = Bid(user=user, value = value, week = week, target = Target.objects.get(id = target))
                 bid.save()
+                targets_dict = {}
+                weekly_bids = []
 
-                self.request.session['visible_targets']= target
-            
+                if self.request.session.get('visible_targets'):
+                    
+                    targets_dict = self.request.session.get('visible_targets')
+                    print("first if", targets_dict)
+                
+                    if targets_dict[str(week)]:
+
+                        weekly_bids = targets_dict[str(week)]
+                        print("second if", targets_dict)
+                        
+                if target not in weekly_bids:
+                    weekly_bids.append(target)
+                targets_dict[str(week)] = weekly_bids
+                print("last", targets_dict)
+                self.request.session['visible_targets']= targets_dict
 
                 return Response(BidSerializer(bid).data, status=status.HTTP_201_CREATED)
             else:
                 return Response({'Bad Request':'Invalid Value or Week'}, status=status.HTTP_400_BAD_REQUEST)
         else:
+
             print(request.data)
             return Response({'Bad Request':'Invalid Bid'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -75,8 +92,10 @@ class UserDidBid(APIView):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
-        
+
         data = {
             'visibleTargets': self.request.session.get('visible_targets')
         }
+        
+        print("OK", data)
         return JsonResponse(data, status=status.HTTP_200_OK)
