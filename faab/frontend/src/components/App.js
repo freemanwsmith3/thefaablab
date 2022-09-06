8609c5c7120fef9be916c7954a8af122ed02ab66
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-06 11:00:15 +0100

    req

--- faab/frontend/src/components/App.js
+++ faab/frontend/src/components/App.js
@@ -4,6 +4,8 @@ import BidPlayer from "./BidPlayer";
 import HomePage from "./HomePage";
 import ShowData from "./ShowData";
 
+
+
 export default class App extends Component {
   constructor(props) {
     super(props);
