977578f2c3c4fa2ccdbd1a95561195ce65a26209
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-06 23:20:59 +0100

    staticfiles

--- faab/faab/settings.py
+++ faab/faab/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
