5b4a9ec3c504a789a919eff20d01cac49553a269
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-06 17:57:09 +0100

    reset2

--- faab/faab/wsgi.py
+++ faab/faab/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faab.settings')
 
+os.environ.setdefault('DJANGO_STRUCTION', 'faab')
 application = get_wsgi_application()
