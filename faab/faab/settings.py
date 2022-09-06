b0cf37404ec2a91a0581e1bbe596a63b1cb03bdc
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-07 00:23:50 +0100

    change static link

--- faab/faab/settings.py
+++ faab/faab/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/staticfiles')
 
-STATIC_URL = '/frontend/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
