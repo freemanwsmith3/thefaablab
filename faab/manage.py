b42c02de3025516db7f43a64a4a1f0e9c471c3de
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-06 20:31:35 +0100

    ok

--- faab/manage.py
+++ faab/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faab.faab.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faab.settings')
     print("OK", os.getcwd())
     try:
         from django.core.management import execute_from_command_line
