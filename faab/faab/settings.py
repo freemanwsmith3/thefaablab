3f6909688ed5d5a1bd5223b800c65448c32936e0
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-06 23:30:05 +0100

    session cookie

--- faab/faab/settings.py
+++ faab/faab/settings.py
@@ -129,3 +129,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#maybe change for heroku
