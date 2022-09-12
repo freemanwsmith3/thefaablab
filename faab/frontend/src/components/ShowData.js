f5ec056fc91bb076667cd244b06ac828bb523e64
Freeman Smith <freemanwsmith3@gmail.com>
2022-09-12 15:07:34 +0100

    react popup

--- faab/frontend/src/components/ShowData.js
+++ faab/frontend/src/components/ShowData.js
@@ -9,7 +9,7 @@ import Card from '@material-ui/core//Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-
+import Popup from "reactjs-popup";
 
 export default class ShowData extends Component {
     constructor(props) {
