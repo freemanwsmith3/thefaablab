7c35282043bda916ac1bdc0b82cd512fd5173687
Freeman Smith <freemanwsmith3@gmail.com>
2023-09-26 11:08:26 -0500

    fix max weeks

--- faab/api/views.py
+++ faab/api/views.py
@@ -108,7 +108,7 @@ class TargetsAPI(APIView):
 
 
         ###################
-        current_week = 16
+        current_week = 18
         ########################
 
         if not self.request.session.exists(self.request.session.session_key):
