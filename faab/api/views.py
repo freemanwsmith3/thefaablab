ae5d576041a5c8dc0fe3efe6ad1ce23ac198c461
Freeman Smith <freemanwsmith3@gmail.com>
2023-11-21 15:03:22 +0100

    update week

--- faab/api/views.py
+++ faab/api/views.py
@@ -109,7 +109,7 @@ class TargetsAPI(APIView):
 
 
         ###################
-        current_week = 24
+        current_week = 26
         ########################
 
         if not self.request.session.exists(self.request.session.session_key):
